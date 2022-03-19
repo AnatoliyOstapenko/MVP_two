879eb2a2dc93d975f90b9c45c52c5668e9d853e7
Anatolii Ostapenko <anatoliy.ostapenko@gmail.com>
2022-03-19 17:20:51 +0200

    add NavigationController and Model class

---
+++ MVP_two/MVP/Model/UserModel.swift
@@ -0,0 +1,52 @@
+//
+//  UserModel.swift
+//  MVP_two
+//
+//  Created by AnatoliiOstapenko on 19.03.2022.
+//
+
+import Foundation
+
+struct UserModel: Decodable {
+    let id: String
+    let name: String
+    let username: String
+    let email: String
+    let address: Address
+    let phone: String
+    let website: String
+    let company: Company
+    
+}
+
+struct Address: Decodable {
+    let street: String
+    let suite: String
+    let city: String
+    let zipcode: String
+    let geo: Geo
+}
+
+struct Geo: Decodable {
+    let latitude: Double
+    let longitude: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case latitude = "lat"
+        case longitude = "lng"
+    }
+}
+
+struct Company: Decodable {
+    let name: String
+    let phrase: String
+    let bs: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name, bs
+        case phrase = "catchPhrase"
+    }
+}
+
+
+
---
+++ MVP_two/MVP/Presenter/Presenter.swift
@@ -0,0 +1,8 @@
+//
+//  Presenter.swift
+//  MVP_two
+//
+//  Created by AnatoliiOstapenko on 19.03.2022.
+//
+
+import Foundation
