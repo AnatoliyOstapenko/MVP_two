7eda13d44180640c55e202179309145614973a83
Anatolii Ostapenko <anatoliy.ostapenko@gmail.com>
2022-03-19 21:24:56 +0200

    Update UserViewController
    Delete Main storyboard
    Add UserViewController storyboard
    Add TableView and Cell

--- MVP_two/MVP/Presenter/Presenter.swift
+++ MVP_two/MVP/Presenter/Presenter.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import UIKit
+
+protocol UserPresenterDelegate: AnyObject {
+    func presentUsers(users: [UserModel])
+    func presentAlert(title: String, message: String)
+}
+
+typealias PresenterDelegate = UserPresenterDelegate & UIViewController
+
+class UserPresenter {
+    weak var userPresenterDelegate: PresenterDelegate?
+    
+    func getUsers() {
+        let urlString = "https://jsonplaceholder.typicode.com/users"
+        guard let url = URL(string: urlString) else { return }
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            guard error == nil, let data = data, let self = self else { return }
+            do {
+                let data = try JSONDecoder().decode([UserModel].self, from: data)
+                self.userPresenterDelegate?.presentUsers(users: data)
+            } catch { print(error.localizedDescription)}
+        }
+        task.resume()
+    }
+    
+}
+
--- MVP_two/MVP/View/UserViewController.swift
+++ MVP_two/MVP/View/UserViewController.swift
@@ -8,12 +8,55 @@
 import UIKit
 
 class UserViewController: UIViewController {
+    
+    public let userTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(userTableView)
+        tableViewConstraints()
+        userTableView.dataSource = self
+        userTableView.delegate = self
+    }
+    // more easiest code to add table view
+//    override func viewDidLayoutSubviews() {
+//        super.viewDidLayoutSubviews()
+//        view.addSubview(userTableView)
+//        userTableView.frame = view.bounds
+//    }
+    
+    func tableViewConstraints() {
+        userTableView.translatesAutoresizingMaskIntoConstraints = false
+        userTableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        userTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        userTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        userTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
     }
 
+}
+// MARK: - TableView DataSource
+extension UserViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "roe is \(indexPath.row)"
+        return cell
+    }
+    
+    
+}
 
+// MARK: - TableView Delegate
+extension UserViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true) // avoid gray background of selected row
+    }
 }
 
